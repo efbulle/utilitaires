efe226d3fa673636bf51883687374955e178e02f
Emmanuel Favre-Bulle <emmanuel.favrebulle@gmail.com>
2021-05-10 18:37:56 +0200

    Restructure conf avec une classe et ajuste tests

--- tests/test_utilitaires.py
+++ tests/test_utilitaires.py
@@ -1,10 +1,33 @@
-import utilitaires.conf as conf
+from pathlib import Path
 import unittest
+from unittest.mock import mock_open, patch
+import utilitaires.conf as conf
 
 
 class TestConf(unittest.TestCase):
-    def test_paths(self):
-        self.assertGreater(len(conf.chemins), 1)
+    def test_fichier_manquant(self):
+        c = conf.Config(["fichier_manquant"])
+        self.assertEqual(c.chemins, {"transformed": Path(".")})
+
+    def test_section_manquante(self):
+        with patch("builtins.open", mock_open(read_data="[test]")):
+            c = conf.Config(["mock"])
+            self.assertEqual(c.chemins, {"transformed": Path(".")})
+
+    def test_valeurs(self):
+        d = {"transformed": "test", "output": "test2"}
+        read_data = "[paths]\n"
+        for k, v in d.items():
+            read_data += f"{k}={v}\n"
+        with patch("builtins.open", mock_open(read_data=read_data)):
+            c = conf.Config(["mock"])
+            self.assertEqual(c.chemins, {k: Path(v) for k, v in d.items()})
+
+    def test_transformed_default(self):
+        read_data = "[paths]\noutput=test"
+        with patch("builtins.open", mock_open(read_data=read_data)):
+            c = conf.Config(["mock"])
+            self.assertEqual(c.chemins["transformed"], Path("."))
 
 
 if __name__ == "__main__":
--- utilitaires/conf.py
+++ utilitaires/conf.py
@@ -1,25 +1,34 @@
-"""Module de lecture d'un fichier de configuration.
+"""Configuration pour les autres modules.
 
-Nommé `projects.ini` et situé dans le répertoire personnel.
-La syntaxe est celle des fichiers .ini :
-https://docs.python.org/3/library/configparser.html#supported-ini-file-structure
-
-Crée un dict 'chemins' contenant les répertoires utiles aux modules sous forme de pathlib.Path.
+Pour l'instant, juste des chemins de repertoires.
+Lit 'projects.ini' situé dans le répertoire personnel 
+'chemins' contient un dict avec les répertoires utiles aux modules.
 """
 
 from pathlib import Path
 import configparser
 
-CONFIG_FILENAME = "projects.ini"
-CONFIG_PATH = Path.home()
-PATH_SECTION_NAME = "paths"
-DEFAULT_PATHS = {PATH_SECTION_NAME: {"transformed": "."}}
 
-config = configparser.ConfigParser()
-config.read_dict(DEFAULT_PATHS)
-config.read([CONFIG_PATH / CONFIG_FILENAME])
-p = config[PATH_SECTION_NAME]
-chemins = {c: Path(p[c]) for c in p}
-for c in chemins.values():
-    if not c.exists():
-        print(f"Le chemin {c} n'existe pas.")
+class Config:
+    """Lecture du fichier de configuration.
+
+    La syntaxe est celle des fichiers .ini :
+    https://docs.python.org/3/library/configparser.html#supported-ini-file-structure
+
+    Attributs:
+        chemins: dict, créé à partir de la section 'paths', contenant des répertoires
+            avec au moins une clé "transformed".
+    """
+
+    def __init__(self, filenames: list[Path]) -> None:
+        config = configparser.ConfigParser()
+        config.read_dict({"paths": {"transformed": "."}})
+        config.read(filenames=filenames)
+        p = config["paths"]
+        self.chemins = {c: Path(p[c]) for c in p}
+        for c in self.chemins.values():
+            if not c.exists():
+                print(f"Le chemin {c} n'existe pas.")
+
+
+chemins = Config([Path.home() / "projects.ini"]).chemins
