35293e350ad937f685de687fce98ea240580aed4
Emmanuel Favre-Bulle <emmanuel.favrebulle@gmail.com>
2021-05-10 16:06:23 +0200

    Réécrit les valeurs par défauts du fichier de configuration

--- utilitaires/conf.py
+++ utilitaires/conf.py
@@ -4,18 +4,22 @@ Nommé `projects.ini` et situé dans le répertoire personnel.
 La syntaxe est celle des fichiers .ini :
 https://docs.python.org/3/library/configparser.html#supported-ini-file-structure
 
-Doit contenir une section Paths. À défaut, les chemins seront fixés au répertoire courant.
+Crée un dict 'chemins' contenant les répertoires utiles aux modules sous forme de pathlib.Path.
 """
+
 from pathlib import Path
 import configparser
 
 CONFIG_FILENAME = "projects.ini"
 CONFIG_PATH = Path.home()
+PATH_SECTION_NAME = "paths"
+DEFAULT_PATHS = {PATH_SECTION_NAME: {"transformed": "."}}
 
 config = configparser.ConfigParser()
+config.read_dict(DEFAULT_PATHS)
 config.read([CONFIG_PATH / CONFIG_FILENAME])
-p = config["Paths"]
-chemins = {c: Path(p.get(c, ".")) for c in p}
+p = config[PATH_SECTION_NAME]
+chemins = {c: Path(p[c]) for c in p}
 for c in chemins.values():
     if not c.exists():
-        print(f"{c} n'existe pas.")
+        print(f"Le chemin {c} n'existe pas.")
